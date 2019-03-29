72cf655174ea17f193b70de742385729daf4e041
wu en <wuyunen@dyn-118-139-24-71.its.monash.edu.au>
2019-03-29 12:03:23 +1100

    Initial Commit

---
+++ SmartStatis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartStatis
+//
+//  Created by wu yun en on 2019/3/29.
+//  Copyright © 2019 GenX Leader. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
